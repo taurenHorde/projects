a398fd06eded3c9a48f7042f4e1ea6f413cc5a23
Tauren <118504053+taurenHorde@users.noreply.github.com>
2024-09-30 21:49:33 +0900

    Delete static/js directory

--- static/js/react-syntax-highlighter/react-syntax-highlighter_languages_refractor_bnf.d0d322b6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknewpf=self.webpackChunknewpf||[]).push([[1126],{3605:n=>{function e(n){n.languages.bnf={string:{pattern:/"[^\r\n"]*"|'[^\r\n']*'/},definition:{pattern:/<[^<>\r\n\t]+>(?=\s*::=)/,alias:["rule","keyword"],inside:{punctuation:/^<|>$/}},rule:{pattern:/<[^<>\r\n\t]+>/,inside:{punctuation:/^<|>$/}},operator:/::=|[|()[\]{}*+?]|\.{3}/},n.languages.rbnf=n.languages.bnf}n.exports=e,e.displayName="bnf",e.aliases=["rbnf"]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_bnf.d0d322b6.chunk.js.map
